a6f518c34acc09ca6bfe40e6df2bf930dd4dc904
Daniel Connelly <dhconnelly@gmail.com>
2025-02-17 16:31:55 -0800

    apply test suite overrides

--- Makefile
+++ Makefile
@@ -10,4 +10,6 @@ build:
 
 test:
 	mvn test
+	./overrides/apply_overrides.sh craftinginterpreters
 	cd craftinginterpreters; dart tool/bin/test.dart chap10_functions --interpreter ../lox
+	cd craftinginterpreters; git checkout -f
--- overrides/apply_overrides.sh
+++ overrides/apply_overrides.sh
@@ -3,10 +3,14 @@
 set -e
 
 BOOK_DIR=$1
-THIS_DIR=$(dirname "$@")
-OVERRIDES=unexpected_character.lox for/statement_condition.lox for/statement_initializer.lox
+if [ -z "$BOOK_DIR" ]; then
+  echo "usage: apply_overrides BOOK_DIR";
+  exit 1;
+fi
+THIS_DIR="$(dirname "$0")"
+OVERRIDES="unexpected_character.lox for/statement_condition.lox for/statement_initializer.lox"
 
 for override in $OVERRIDES; do
-  rm "$BOOK_DIR"/"$override";
-  cp "$THIS_DIR"/"$override" "$BOOK_DIR"/"$override";
+  rm "$BOOK_DIR"/test/"$override";
+  cp "$THIS_DIR"/"$override" "$BOOK_DIR"/test/"$override";
 done
